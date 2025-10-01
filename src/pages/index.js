277a8797ca41a2a47d9190a5d37ab328cbe44770
Francisco Murillo <franciscomurillo@Franciscos-MacBook-Air.local>
2025-09-30 20:41:11 -0600

    starting

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,15 +27,13 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
+            Hola
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/pages/index.js
+              
             </code>
             .
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
+          
         </ol>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
